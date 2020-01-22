b1fc46c4a43eb7dd281fcead015b21b590a842e8
Szwaczyn <michal.szwaczyk@outlook.com>
2020-01-22 18:01:31 +0100

    HTML Response

--- src/javaee/servlets/HelloWorldHTML.java
+++ src/javaee/servlets/HelloWorldHTML.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorld extends HttpServlet {
-	
+public class HelloWorldHTML extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-		response.setContentType("text/plain; charset=utf-8");
+		response.setContentType("text/html charset=utf-8");
 		PrintWriter pw = response.getWriter();
-		pw.println("Hello World ! ś");
+		
+		pw.println("<head> <title> Hello world </title> </head>");
+		pw.println("<body> <h1>Hello World </h1> </body>");
 	}
 }
