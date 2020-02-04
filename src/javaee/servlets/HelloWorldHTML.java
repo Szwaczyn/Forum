c1187ddca6337aaa508fc7d405c8c30af559de75
Szwaczyn <michal.szwaczyk@outlook.com>
2020-02-04 21:02:34 +0100

    Dispatcher

--- src/javaee/servlets/HelloWorldHTML.java
+++ src/javaee/servlets/HelloWorldHTML.java
@@ -3,14 +3,20 @@ package javaee.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldHTML extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
+		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp"); // Pobranie dispatchera
+		rd.include(request, response); // Dołączenie żądania so serwleu.
+		rd.forward(request, response); // Przekazanie żądania do serwletu.
+		
 		response.setContentType("text/html charset=utf-8");
 		PrintWriter pw = response.getWriter();
 		
